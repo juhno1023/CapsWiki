4b71fd59a23e8accd7ad2e323ef50bf41532c23c
Jinsoo Yoon <floreo1242@gmail.com>
2024-02-27 17:14:22 +0900

    api 매핑 수정

--- capsWiki/src/main/java/com/example/capswiki/Domain/Post/Controller/PostController.java
+++ capsWiki/src/main/java/com/example/capswiki/Domain/Post/Controller/PostController.java
@@ -17,13 +17,13 @@ public class PostController {
     @Autowired
     private PostService postService;
 
-    @PostMapping("/recruitPost") // 글 작성
+    @PostMapping("/post") // 글 작성
     // ResponseEntity를 통해 요청에 대한 응답을 보냄
-    public ResponseEntity<?> createRecruitPostAndTeam(@RequestBody PostRequestDTO recruitPostDTO) {
+    public ResponseEntity<?> createPost(@RequestBody PostRequestDTO postRequestDTO) {
         //위 RequestBody를 통해 전달된 DTO를 통해 클라이언트의 요청을 처리함
         try {
-            postService.createPost(recruitPostDTO); //서비스의 createPost 기능을 사용
-            return ResponseEntity.ok("Successfully posted recruit Post"); // 성공적으로 수행할 시, 해당 메세지 반환
+            postService.createPost(postRequestDTO); //서비스의 createPost 기능을 사용
+            return ResponseEntity.ok("Successfully posted a post"); // 성공적으로 수행할 시, 해당 메세지 반환
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage()); //실패 시 오류 메세지 반환
         }
