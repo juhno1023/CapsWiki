afca038a9a06b830ad214927a4ff4b0daa0abcb6
juhno1023 <hyeondnr1023@gmail.com>
2024-02-27 15:53:48 +0900

    게시글 작성 기능 완성

--- capsWiki/src/main/java/com/example/capswiki/Domain/Post/Controller/PostController.java
+++ capsWiki/src/main/java/com/example/capswiki/Domain/Post/Controller/PostController.java
@@ -1,4 +1,36 @@
 package com.example.capswiki.Domain.Post.Controller;
 
+import com.example.capswiki.DTO.Post.RequestDTO.PostRequestDTO;
+import com.example.capswiki.DTO.Post.ResponseDTO.PostResponseDTO;
+import com.example.capswiki.Domain.Post.Service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("/recruitPost") // 글 작성
+    // ResponseEntity를 통해 요청에 대한 응답을 보냄
+    public ResponseEntity<?> createRecruitPostAndTeam(@RequestBody PostRequestDTO recruitPostDTO) {
+        //위 RequestBody를 통해 전달된 DTO를 통해 클라이언트의 요청을 처리함
+        try {
+            postService.createPost(recruitPostDTO); //서비스의 createPost 기능을 사용
+            return ResponseEntity.ok("Successfully posted recruit Post"); // 성공적으로 수행할 시, 해당 메세지 반환
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage()); //실패 시 오류 메세지 반환
+        }
+    }
+
+//    @GetMapping("/Post/{PostId}") // 게시글 조회
+//    public ResponseEntity<PostResponseDTO> getRecruitPost(@PathVariable int recruitPostId){
+//        return ResponseEntity.ok(postService.getRecruitPost(recruitPostId));
+//    }
 }
--- capsWiki/src/main/java/com/example/capswiki/Domain/Post/Repository/PostRepository.java
+++ capsWiki/src/main/java/com/example/capswiki/Domain/Post/Repository/PostRepository.java
@@ -1,4 +1,17 @@
 package com.example.capswiki.Domain.Post.Repository;
 
-public class PostRepository {
+import com.example.capswiki.DAO.Post.Post;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+//    Post findPostByTitle(String title) //제목으로 검색
+    Post save(Post post); // 글 저장
+
 }
